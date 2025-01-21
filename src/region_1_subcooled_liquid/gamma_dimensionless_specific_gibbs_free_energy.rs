d85bf3a6b9db1775fff1c2ebf29fc4a654e13474
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-21 16:41:22 +0800

    region one derivatives copied over and dimenisoned with temp and pressure

--- src/region_1_subcooled_liquid/gamma_dimensionless_specific_gibbs_free_energy.rs
+++ src/region_1_subcooled_liquid/gamma_dimensionless_specific_gibbs_free_energy.rs
@@ -1,50 +1,13 @@
 
-pub const REGION_1_COEFFS: [[f64; 3]; 34] = [
-    [0.0, -2.0, 0.14632971213167],
-    [0.0, -1.0, -0.84548187169114],
-    [0.0, 0.0, -0.37563603672040e1],
-    [0.0, 1.0, 0.33855169168385e1],
-    [0.0, 2.0, -0.95791963387872],
-    [0.0, 3.0, 0.15772038513228],
-    [0.0, 4.0, -0.16616417199501e-1],
-    [0.0, 5.0, 0.81214629983568e-3],
-    [1.0, -9.0, 0.28319080123804e-3],
-    [1.0, -7.0, -0.60706301565874e-3],
-    [1.0, -1.0, -0.18990068218419e-1],
-    [1.0, 0.0, -0.32529748770505e-1],
-    [1.0, 1.0, -0.21841717175414e-1],
-    [1.0, 3.0, -0.52838357969930e-4],
-    [2.0, -3.0, -0.47184321073267e-3],
-    [2.0, 0.0, -0.30001780793026e-3],
-    [2.0, 1.0, 0.47661393906987e-4],
-    [2.0, 3.0, -0.44141845330846e-5],
-    [2.0, 17.0, -0.72694996297594e-15],
-    [3.0, -4.0, -0.31679644845054e-4],
-    [3.0, 0.0, -0.28270797985312e-5],
-    [3.0, 6.0, -0.85205128120103e-9],
-    [4.0, -5.0, -0.22425281908000e-5],
-    [4.0, -2.0, -0.65171222895601e-6],
-    [4.0, 10.0, -0.14341729937924e-12],
-    [5.0, -8.0, -0.40516996860117e-6],
-    [8.0, -11.0, -0.12734301741641e-8],
-    [8.0, -6.0, -0.17424871230634e-9],
-    [21.0, -29.0, -0.68762131295531e-18],
-    [23.0, -31.0, 0.14478307828521e-19],
-    [29.0, -38.0, 0.26335781662795e-22],
-    [30.0, -39.0, -0.11947622640071e-22],
-    [31.0, -40.0, 0.18228094581404e-23],
-    [32.0, -41.0, -0.93537087292458e-25],
-];
-
 // ================    Region 1 ===================
 
 use uom::si::f64::*;
 
-use super::{pi_1, tau_1};
+use super::{pi_1, tau_1, REGION_1_COEFFS};
 /// Returns the region-1 gamma
 /// Temperature is assumed to be in K
 /// Pressure is assumed to be in Pa
-fn gamma_1(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+pub fn gamma_1(t: ThermodynamicTemperature, p: Pressure) -> f64 {
     let tau = tau_1(t);
     let pi = pi_1(p);
     let mut sum = 0.0;
@@ -57,20 +20,5 @@ fn gamma_1(t: ThermodynamicTemperature, p: Pressure) -> f64 {
     sum
 }
 
-/// Returns the region-1 gamma_pi
-/// Temperature is assumed to be in K
-/// Pressure is assumed to be in Pa
-fn gamma_pi_1(t: ThermodynamicTemperature, p: Pressure) -> f64 {
-    let tau = tau_1(t);
-    let pi = pi_1(p);
-    let mut sum = 0.0;
-    for coefficient in REGION_1_COEFFS {
-        let ii = coefficient[0] as i32;
-        let ji = coefficient[1] as i32;
-        let ni = coefficient[2];
-        sum += -ni * f64::from(ii) * (7.1 - pi).powi(ii - 1) * (tau - 1.222).powi(ji);
-    }
-    sum
-}
 
 
--- src/region_1_subcooled_liquid/mod.rs
+++ src/region_1_subcooled_liquid/mod.rs
@@ -1,4 +1,41 @@
 
+pub const REGION_1_COEFFS: [[f64; 3]; 34] = [
+    [0.0, -2.0, 0.14632971213167],
+    [0.0, -1.0, -0.84548187169114],
+    [0.0, 0.0, -0.37563603672040e1],
+    [0.0, 1.0, 0.33855169168385e1],
+    [0.0, 2.0, -0.95791963387872],
+    [0.0, 3.0, 0.15772038513228],
+    [0.0, 4.0, -0.16616417199501e-1],
+    [0.0, 5.0, 0.81214629983568e-3],
+    [1.0, -9.0, 0.28319080123804e-3],
+    [1.0, -7.0, -0.60706301565874e-3],
+    [1.0, -1.0, -0.18990068218419e-1],
+    [1.0, 0.0, -0.32529748770505e-1],
+    [1.0, 1.0, -0.21841717175414e-1],
+    [1.0, 3.0, -0.52838357969930e-4],
+    [2.0, -3.0, -0.47184321073267e-3],
+    [2.0, 0.0, -0.30001780793026e-3],
+    [2.0, 1.0, 0.47661393906987e-4],
+    [2.0, 3.0, -0.44141845330846e-5],
+    [2.0, 17.0, -0.72694996297594e-15],
+    [3.0, -4.0, -0.31679644845054e-4],
+    [3.0, 0.0, -0.28270797985312e-5],
+    [3.0, 6.0, -0.85205128120103e-9],
+    [4.0, -5.0, -0.22425281908000e-5],
+    [4.0, -2.0, -0.65171222895601e-6],
+    [4.0, 10.0, -0.14341729937924e-12],
+    [5.0, -8.0, -0.40516996860117e-6],
+    [8.0, -11.0, -0.12734301741641e-8],
+    [8.0, -6.0, -0.17424871230634e-9],
+    [21.0, -29.0, -0.68762131295531e-18],
+    [23.0, -31.0, 0.14478307828521e-19],
+    [29.0, -38.0, 0.26335781662795e-22],
+    [30.0, -39.0, -0.11947622640071e-22],
+    [31.0, -40.0, 0.18228094581404e-23],
+    [32.0, -41.0, -0.93537087292458e-25],
+];
+
 pub const REGION_1_BACK_COEFFS_PH: [[f64; 3]; 20] = [
     [0.0, 0.0, -0.23872489924521e+3],
     [0.0, 1.0, 0.40421188637945e+3],
@@ -46,6 +83,19 @@ const REGION_1_BACK_COEFFS_PS: [[f64; 3]; 20] = [
 
 pub mod gamma_dimensionless_specific_gibbs_free_energy;
 pub use gamma_dimensionless_specific_gibbs_free_energy::*;
+
+/// derivatives for dimensionless gibbs free energy
+/// used to calculate specific volume, entropy,
+/// internal energy, enthalpy,
+/// cp, cv 
+/// speed of sound 
+/// isentropic exopnent
+///
+/// isobaric cubic exapnsion coeff 
+/// isothermal compressibility
+pub mod gamma_derivatives;
+pub use gamma_derivatives::*;
+
 use uom::si::f64::*;
 use uom::si::pressure::pascal;
 use uom::si::thermodynamic_temperature::kelvin;
@@ -59,7 +109,7 @@ pub fn tau_1(t: ThermodynamicTemperature) -> f64 {
     1386.0 / t_kelvin
 }
 
-/// Returns the region-1 pi
+/// Returns the region-1 pi (dimensionless pressure)
 /// Temperature is assumed to be in K
 pub fn pi_1(p: Pressure) -> f64 {
 
