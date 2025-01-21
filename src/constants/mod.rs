b7699476388a7cbd3286a60ffaea37a387560ccc
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-21 16:21:31 +0800

    added region 1,2,3,5 coeffs added

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,6 +1,3 @@
-use uom::si::f64::*;
-
-
 pub const R_KJ_PER_KG_KELVIN: f64 = 0.461526;
 pub const T_C_KELVIN: f64 = 647.096;
 pub const P_C_MPA: f64 = 22.064;
--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,33 @@ mod tests {
     }
 }
 
-/// constants for the 
+/// constants for the steam table calculations
 pub mod constants;
 
+/// region 1 
+///
+/// Temperature from 273.15 to 623.15 K 
+/// Pressure from 0 to 100 MPa
+///
+/// Up to the saturation line. 
+/// This I believe is subcooled liquid region
+pub mod region_1_subcooled_liquid;
+
+
+
+/// region 2 
+///
+/// Saturated water region
+pub mod region_2_saturated_water;
+
+/// region 3 
+///
+/// vapour liquid equilibrium
+/// wet steam region 
+pub mod region_3_vap_liq_mixture;
+
+
+/// region 5 
+///
+/// superheated steam region 
+pub mod region_5_superheated_steam;
