1357094ce6f068bba512ab5290f00d8bf579ba09
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-21 16:10:27 +0800

    added triple point, boiling point and molar properties of water

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -5,3 +5,14 @@ pub const R_KJ_PER_KG_KELVIN: f64 = 0.461526;
 pub const T_C_KELVIN: f64 = 647.096;
 pub const P_C_MPA: f64 = 22.064;
 pub const RHO_C_KG_PER_M3: f64 = 322.0;
+
+
+pub const T_TRIPLE_PT_KELVIN: f64 = 273.16;
+pub const P_TRIPLE_PT_PASCAL: f64 = 611.657;
+
+pub const T_NORMAL_BP_KELVIN: f64 = 373.1243;
+pub const P_NORMAL_BP_MPA: f64 = 0.101325;
+
+
+pub const MOLAR_MASS_WATER_G_PER_GMOL: f64 = 18.015257;
+pub const R_M_J_PER_MOL_KELVIN: f64 = 8.31451;
