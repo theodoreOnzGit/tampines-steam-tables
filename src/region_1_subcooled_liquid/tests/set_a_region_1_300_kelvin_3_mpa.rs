6aca951b45b78b47e9f22b8626ef21e11fdf9506
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-22 13:59:47 +0800

    specific enthalpy test ok

--- src/region_1_subcooled_liquid/tests/set_a_region_1_300_kelvin_3_mpa.rs
+++ src/region_1_subcooled_liquid/tests/set_a_region_1_300_kelvin_3_mpa.rs
@@ -1,9 +1,9 @@
-use uom::si::pressure::megapascal;
+use uom::si::{available_energy::kilojoule_per_kilogram, pressure::megapascal};
 use uom::si::specific_volume::cubic_meter_per_kilogram;
 use uom::si::thermodynamic_temperature::kelvin;
 use uom::si::f64::*;
 
-use crate::region_1_subcooled_liquid::v_tp_1;
+use crate::region_1_subcooled_liquid::{h_tp_1, v_tp_1};
 
 #[test] 
 pub fn specific_vol_regression_set_a(){
@@ -21,3 +21,21 @@ pub fn specific_vol_regression_set_a(){
 
     
 }
+
+#[test] 
+pub fn specific_enthalpy_regression_set_a(){
+    let ref_vol_kj_per_kg = 0.115331273e3;
+    let t = ThermodynamicTemperature::new::<kelvin>(300.0);
+    let p = Pressure::new::<megapascal>(3.0);
+
+    let specific_vol_test_kj_per_kg = 
+        h_tp_1(t, p).get::<kilojoule_per_kilogram>();
+
+    approx::assert_relative_eq!(
+        ref_vol_kj_per_kg,
+        specific_vol_test_kj_per_kg,
+        max_relative=1e-9);
+
+    
+}
+
