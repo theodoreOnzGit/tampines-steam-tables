c2e55827457203ec81c840ea933cd11ae3c8e285
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-22 18:56:47 +0800

    added gamma derivatives for ideal gas and residual

--- src/region_2_saturated_water/gamma_ideal_gas_plus_derivatives.rs
+++ src/region_2_saturated_water/gamma_ideal_gas_plus_derivatives.rs
@@ -16,3 +16,39 @@ fn gamma_2_ideal(t: ThermodynamicTemperature, p: Pressure) -> f64 {
     }
     pi.ln() + sum
 }
+
+
+/// Returns the region-2 ideal gamma_tau
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_tau_2_ideal(t: ThermodynamicTemperature, _p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    for coefficient in REGION_2_COEFFS_IDEAL {
+        let ji = coefficient[0];
+        let ni = coefficient[1];
+        sum += ni * ji * tau.powf(ji - 1.0);
+    }
+    sum
+}
+
+/// Returns the region-2 ideal gamma_tau_tau
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_tau_tau_2_ideal(t: ThermodynamicTemperature, _p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    for coefficient in REGION_2_COEFFS_IDEAL {
+        let ji = coefficient[0];
+        let ni = coefficient[1];
+        sum += ni * ji * (ji - 1.0) * tau.powf(ji - 2.0);
+    }
+    sum
+}
+
+/// Returns the region-2 ideal gamma_pi
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_pi_2_ideal(_t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    1.0 / pi_2(p)
+}
--- src/region_2_saturated_water/gamma_residual_plus_derivatives.rs
+++ src/region_2_saturated_water/gamma_residual_plus_derivatives.rs
@@ -17,3 +17,84 @@ fn gamma_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
     }
     sum
 }
+
+
+/// Returns the region-2 residual gamma_tau
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_tau_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    let pi = pi_2(p);
+    for coefficient in REGION_2_COEFFS_RES {
+        let ii = coefficient[0] as i32;
+        let ji = coefficient[1] as i32;
+        let ni = coefficient[2];
+        sum += ni * pi.powi(ii) * f64::from(ji) * (tau - 0.5).powi(ji - 1);
+    }
+    sum
+}
+
+/// Returns the region-2 residual gamma_tau_tau
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_tau_tau_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    let pi = pi_2(p);
+    for coefficient in REGION_2_COEFFS_RES {
+        let ii = coefficient[0] as i32;
+        let ji = coefficient[1] as i32;
+        let ni = coefficient[2];
+        sum += ni * pi.powi(ii) * f64::from(ji * (ji - 1)) * (tau - 0.5).powi(ji - 2);
+    }
+    sum
+}
+
+/// Returns the region-2 residual gamma_pi
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_pi_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    let pi = pi_2(p);
+    for coefficient in REGION_2_COEFFS_RES {
+        let ii = coefficient[0] as i32;
+        let ji = coefficient[1] as i32;
+        let ni = coefficient[2];
+        sum += ni * pi.powi(ii - 1) * f64::from(ii) * (tau - 0.5).powi(ji);
+    }
+    sum
+}
+
+/// Returns the region-2 residual gamma_pi_pi
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_pi_pi_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    let pi = pi_2(p);
+    for coefficient in REGION_2_COEFFS_RES {
+        let ii = coefficient[0] as i32;
+        let ji = coefficient[1] as i32;
+        let ni = coefficient[2];
+        sum += ni * pi.powi(ii - 2) * f64::from(ii * (ii - 1)) * (tau - 0.5).powi(ji);
+    }
+    sum
+}
+
+/// Returns the region-2 residual gamma_pi_tau
+/// Temperature is assumed to be in K
+/// Pressure is assumed to be in Pa
+fn gamma_pi_tau_2_res(t: ThermodynamicTemperature, p: Pressure) -> f64 {
+    let mut sum: f64 = 0.0;
+    let tau = tau_2(t);
+    let pi = pi_2(p);
+    for coefficient in REGION_2_COEFFS_RES {
+        let ii = coefficient[0] as i32;
+        let ji = coefficient[1] as i32;
+        let ni = coefficient[2];
+        sum += ni * pi.powi(ii - 1) * f64::from(ii * ji) * (tau - 0.5).powi(ji - 1);
+    }
+    sum
+}
