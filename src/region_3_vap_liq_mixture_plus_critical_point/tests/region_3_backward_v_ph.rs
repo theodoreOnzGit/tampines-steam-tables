803dbbb4365fc1f912e271abc4c1116f7dac2bef
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2025-01-24 16:57:54 +0800

    vph flash 3b needs typo correction

--- src/region_3_vap_liq_mixture_plus_critical_point/tests/region_3_backward_v_ph.rs
+++ src/region_3_vap_liq_mixture_plus_critical_point/tests/region_3_backward_v_ph.rs
@@ -1,6 +1,6 @@
 use uom::si::{available_energy::kilojoule_per_kilogram, f64::*, pressure::megapascal, specific_volume::cubic_meter_per_kilogram};
 
-use crate::region_3_vap_liq_mixture_plus_critical_point::v_ph_3a;
+use crate::region_3_vap_liq_mixture_plus_critical_point::{v_ph_3a, v_ph_3b};
 #[test] 
 pub fn v3a_ph_test1(){
     let p = Pressure::new::<megapascal>(20.0);
@@ -44,6 +44,59 @@ pub fn v3a_ph_test3(){
 
     let v_test_m3_per_kg = 
         v_ph_3a(p, h)
+        .get::<cubic_meter_per_kilogram>();
+
+    approx::assert_relative_eq!(
+        v_verification_m3_per_kg,
+        v_test_m3_per_kg,
+        max_relative=1e-8
+        );
+}
+
+
+#[test] 
+pub fn v3b_ph_test1(){
+    let p = Pressure::new::<megapascal>(20.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2500.0);
+    let v_verification_m3_per_kg = 6.670547043e-3;
+
+
+    let v_test_m3_per_kg = 
+        v_ph_3b(p, h)
+        .get::<cubic_meter_per_kilogram>();
+
+    approx::assert_relative_eq!(
+        v_verification_m3_per_kg,
+        v_test_m3_per_kg,
+        max_relative=1e-8
+        );
+}
+#[test] 
+pub fn v3b_ph_test2(){
+    let p = Pressure::new::<megapascal>(50.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2400.0);
+    let v_verification_m3_per_kg = 2.801244590e-3;
+
+
+    let v_test_m3_per_kg = 
+        v_ph_3b(p, h)
+        .get::<cubic_meter_per_kilogram>();
+
+    approx::assert_relative_eq!(
+        v_verification_m3_per_kg,
+        v_test_m3_per_kg,
+        max_relative=1e-8
+        );
+}
+#[test] 
+pub fn v3b_ph_test3(){
+    let p = Pressure::new::<megapascal>(100.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2700.0);
+    let v_verification_m3_per_kg = 2.404234998e-3;
+
+
+    let v_test_m3_per_kg = 
+        v_ph_3b(p, h)
         .get::<cubic_meter_per_kilogram>();
 
     approx::assert_relative_eq!(
