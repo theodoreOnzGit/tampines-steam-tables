4b0e7d5085831654785d37d6a2a4da04669fc8e9
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2025-01-24 11:08:18 +0800

    reorged 2b2c boundary tests

--- src/region_2_vapour/tests/region_2_t_ph_flash/mod.rs
+++ src/region_2_vapour/tests/region_2_t_ph_flash/mod.rs
@@ -4,38 +4,8 @@ use uom::si::f64::*;
 use uom::si::thermodynamic_temperature::kelvin;
 
 use crate::region_2_vapour::t_ph_2;
-use crate::region_2_vapour::{h_2b2c, p_2b2c};
 
-#[test]
-pub fn boundary_2b2c_pressure(){
-    // this is the ph point that must be met
-    let p_verification = Pressure::new::<megapascal>(0.100_000_000e3);
-    let h_verification = AvailableEnergy::new::<kilojoule_per_kilogram>(0.351_600_432_3e4);
-
-    let p_test = p_2b2c(h_verification);
-
-    approx::assert_relative_eq!(
-        p_verification.get::<megapascal>(),
-        p_test.get::<megapascal>(),
-        max_relative=1e-8
-        );
-}
-
-#[test]
-pub fn boundary_2b2c_enthalpy(){
-    // this is the ph point that must be met
-    let p_verification = Pressure::new::<megapascal>(0.100_000_000e3);
-    let h_verification = AvailableEnergy::new::<kilojoule_per_kilogram>(0.351_600_432_3e4);
-
-    let h_test = h_2b2c(p_verification);
-
-    approx::assert_relative_eq!(
-        h_verification.get::<kilojoule_per_kilogram>(),
-        h_test.get::<kilojoule_per_kilogram>(),
-        max_relative=1e-8
-        );
-
-}
+pub mod boundary_tests_2b2c;
 
 
 #[test]
