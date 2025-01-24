2b4fb67cfbf36bab5dd3c5adace88c548ff7091f
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2025-01-24 13:25:35 +0800

    region 2 ph back equations fully tested

--- src/region_2_vapour/tests/region_2_t_ph_flash/mod.rs
+++ src/region_2_vapour/tests/region_2_t_ph_flash/mod.rs
@@ -10,7 +10,7 @@ pub mod boundary_tests_2b2c;
 
 #[test]
 pub fn subregion_2a_t_ph_test_1(){
-    let p = Pressure::new::<megapascal>(1.0);
+    let p = Pressure::new::<megapascal>(0.001);
     let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
     let t_expected_kelvin = 0.534433241e3;
 
@@ -29,7 +29,7 @@ pub fn subregion_2a_t_ph_test_1(){
 pub fn subregion_2a_t_ph_test_2(){
     let p = Pressure::new::<megapascal>(3.0);
     let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let t_expected_kelvin = 0.575373370e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -39,13 +39,12 @@ pub fn subregion_2a_t_ph_test_2(){
         max_relative=1e-8
         );
 
-    todo!()
 }
 #[test]
 pub fn subregion_2a_t_ph_test_3(){
-    let p = Pressure::new::<megapascal>(1.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(3.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(4000.0);
+    let t_expected_kelvin = 0.101077577e4;
 
     let t_test = t_ph_2(p, h);
 
@@ -55,14 +54,13 @@ pub fn subregion_2a_t_ph_test_3(){
         max_relative=1e-8
         );
 
-    todo!()
 }
 
 #[test]
 pub fn subregion_2b_t_ph_test_1(){
-    let p = Pressure::new::<megapascal>(1.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(5.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3500.0);
+    let t_expected_kelvin = 0.801299102e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -72,15 +70,14 @@ pub fn subregion_2b_t_ph_test_1(){
         max_relative=1e-8
         );
 
-    todo!()
 }
 
 
 #[test]
 pub fn subregion_2b_t_ph_test_2(){
-    let p = Pressure::new::<megapascal>(3.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(5.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(4000.0);
+    let t_expected_kelvin = 0.101531583e4;
 
     let t_test = t_ph_2(p, h);
 
@@ -90,14 +87,13 @@ pub fn subregion_2b_t_ph_test_2(){
         max_relative=1e-8
         );
 
-    todo!()
 }
 
 #[test]
 pub fn subregion_2b_t_ph_test_3(){
-    let p = Pressure::new::<megapascal>(1.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(25.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3500.0);
+    let t_expected_kelvin = 0.875279054e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -106,16 +102,14 @@ pub fn subregion_2b_t_ph_test_3(){
         t_test.get::<kelvin>(),
         max_relative=1e-8
         );
-
-    todo!()
 }
 
 
 #[test]
 pub fn subregion_2c_t_ph_test_1(){
-    let p = Pressure::new::<megapascal>(1.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(40.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2700.0);
+    let t_expected_kelvin = 0.743056411e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -124,16 +118,14 @@ pub fn subregion_2c_t_ph_test_1(){
         t_test.get::<kelvin>(),
         max_relative=1e-8
         );
-
-    todo!()
 }
 
 
 #[test]
 pub fn subregion_2c_t_ph_test_2(){
-    let p = Pressure::new::<megapascal>(3.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(60.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2700.0);
+    let t_expected_kelvin = 0.791137067e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -142,14 +134,12 @@ pub fn subregion_2c_t_ph_test_2(){
         t_test.get::<kelvin>(),
         max_relative=1e-8
         );
-
-    todo!()
 }
 #[test]
 pub fn subregion_2c_t_ph_test_3(){
-    let p = Pressure::new::<megapascal>(1.0);
-    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3000.0);
-    let t_expected_kelvin = 0.534433241e3;
+    let p = Pressure::new::<megapascal>(60.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3200.0);
+    let t_expected_kelvin = 0.882756860e3;
 
     let t_test = t_ph_2(p, h);
 
@@ -159,5 +149,4 @@ pub fn subregion_2c_t_ph_test_3(){
         max_relative=1e-8
         );
 
-    todo!()
 }
