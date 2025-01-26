896bd772bbba27588b183f9b6e86fd0b8b5e2fd5
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-26 23:07:01 +0800

    added forward eqns for tp flash (mostly done except region 3, which requires backward eqns)

--- src/interfaces/functional_programming/pt_flash_eqm/mod.rs
+++ src/interfaces/functional_programming/pt_flash_eqm/mod.rs
@@ -1,6 +1,6 @@
 use uom::si::{f64::*, pressure::pascal, thermodynamic_temperature::kelvin};
 
-use crate::{region_1_subcooled_liquid::h_tp_1, region_2_vapour::h_tp_2, region_3_single_phase_plus_supercritical_steam::p_boundary_2_3, region_4_vap_liq_equilibrium::sat_pressure_4, region_5_superheated_steam::h_tp_5};
+use crate::{region_1_subcooled_liquid::{cp_tp_1, cv_tp_1, h_tp_1, s_tp_1, u_tp_1, v_tp_1, w_tp_1}, region_2_vapour::{cp_tp_2, cv_tp_2, h_tp_2, s_tp_2, u_tp_2, v_tp_2, w_tp_2}, region_3_single_phase_plus_supercritical_steam::p_boundary_2_3, region_4_vap_liq_equilibrium::sat_pressure_4, region_5_superheated_steam::{cp_tp_5, cv_tp_5, h_tp_5, s_tp_5, u_tp_5, v_tp_5, w_tp_5}};
 
 /// an enum to help represent the appropriate 
 /// regions in the forward equations
@@ -73,5 +73,89 @@ pub fn h_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> AvailableEnergy {
         FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
         FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
         FwdEqnRegion::Region5 => h_tp_5(t, p),
+    }
+}
+
+/// returns the internal energy given temperature and pressure
+pub fn u_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> AvailableEnergy {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => u_tp_1(t, p),
+        FwdEqnRegion::Region2 => u_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => u_tp_5(t, p),
+    }
+}
+
+
+/// returns the specific entropy given temperature and pressure
+pub fn s_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> SpecificHeatCapacity {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => s_tp_1(t, p),
+        FwdEqnRegion::Region2 => s_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => s_tp_5(t, p),
+    }
+}
+
+/// returns the isobaric (const pressure) heat capacitygiven temperature and pressure
+pub fn cp_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> SpecificHeatCapacity {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => cp_tp_1(t, p),
+        FwdEqnRegion::Region2 => cp_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => cp_tp_5(t, p),
+    }
+}
+
+
+/// returns the isochoric (const vol) heat capacity given temperature and pressure
+pub fn cv_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> SpecificHeatCapacity {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => cv_tp_1(t, p),
+        FwdEqnRegion::Region2 => cv_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => cv_tp_5(t, p),
+    }
+}
+
+
+
+
+/// returns the specific volume given temperature and pressure
+pub fn v_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> SpecificVolume {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => v_tp_1(t, p),
+        FwdEqnRegion::Region2 => v_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => v_tp_5(t, p),
+    }
+}
+
+
+/// returns the speed of sound given temperature and pressure
+pub fn w_tp_eqm(t: ThermodynamicTemperature, p: Pressure) -> Velocity {
+    let region = region_fwd_eqn(t, p);
+
+    match region {
+        FwdEqnRegion::Region1 => w_tp_1(t, p),
+        FwdEqnRegion::Region2 => w_tp_2(t, p),
+        FwdEqnRegion::Region3 => todo!("(t,p) flash for region 3 not yet implemented"),
+        FwdEqnRegion::Region4 => todo!("cannot find enthalpy of mixture without steam quality"),
+        FwdEqnRegion::Region5 => w_tp_5(t, p),
     }
 }
