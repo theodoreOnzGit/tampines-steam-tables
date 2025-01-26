065f293d24dfc2bd16ab11235c0bca0aaca34f99
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-26 19:13:08 +0800

    t ph equations for region 3 complete with test

--- src/region_3_vap_liq_mixture_plus_critical_point/tests/region_3_backward_t_ph.rs
+++ src/region_3_vap_liq_mixture_plus_critical_point/tests/region_3_backward_t_ph.rs
@@ -1,6 +1,60 @@
 use uom::si::{available_energy::kilojoule_per_kilogram, f64::*, pressure::megapascal, specific_volume::cubic_meter_per_kilogram, thermodynamic_temperature::kelvin};
 
-use crate::region_3_vap_liq_mixture_plus_critical_point::t_ph_3b;
+use crate::region_3_vap_liq_mixture_plus_critical_point::{t_ph_3a, t_ph_3b};
+#[test] 
+pub fn t3a_ph_test1(){
+    let p = Pressure::new::<megapascal>(20.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1700.0);
+    let t_verification_kelvin = 6.293083892e2;
+
+
+    let t_test_kelvin = 
+        t_ph_3a(p, h)
+        .get::<kelvin>();
+
+    approx::assert_relative_eq!(
+        t_verification_kelvin,
+        t_test_kelvin,
+        max_relative=1e-8
+        );
+}
+
+#[test] 
+pub fn t3a_ph_test2(){
+    let p = Pressure::new::<megapascal>(50.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2000.0);
+    let t_verification_kelvin = 6.905718338e2;
+
+
+    let t_test_kelvin = 
+        t_ph_3a(p, h)
+        .get::<kelvin>();
+
+    approx::assert_relative_eq!(
+        t_verification_kelvin,
+        t_test_kelvin,
+        max_relative=1e-8
+        );
+}
+
+#[test] 
+pub fn t3a_ph_test3(){
+    let p = Pressure::new::<megapascal>(100.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2100.0);
+    let t_verification_kelvin = 7.336163014e2;
+
+
+    let t_test_kelvin = 
+        t_ph_3a(p, h)
+        .get::<kelvin>();
+
+    approx::assert_relative_eq!(
+        t_verification_kelvin,
+        t_test_kelvin,
+        max_relative=1e-8
+        );
+}
+
 
 #[test] 
 pub fn t3b_ph_test1(){
