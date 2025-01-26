1e74a502a9871e02ec891525284b890c877b6a8d
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-27 00:10:17 +0800

    added bounding checks for ph flash

--- src/interfaces/functional_programming/ph_flash_eqm/mod.rs
+++ src/interfaces/functional_programming/ph_flash_eqm/mod.rs
@@ -9,10 +9,22 @@ use super::pt_flash_eqm::{h_tp_eqm, FwdEqnRegion};
 // note that ph flash does not work in region 5
 pub(crate) fn ph_flash_region(p: Pressure, h: AvailableEnergy) -> FwdEqnRegion {
 
+    check_if_within_ph_validity_region(p, h);
     todo!()
+}
 
+/// panics if outside validity region
+fn check_if_within_ph_validity_region(p: Pressure, h: AvailableEnergy,){
+    if is_outside_pressure_range(p) {
+        panic!("p,h point is outside pressure range");
+    };
 
-
+    if is_below_isotherm_t_273_15(p, h) {
+        panic!("p,h point below 273.15K");
+    };
+    if is_above_isotherm_t_1073_15(p, h) {
+        panic!("p,h point above 1073.15K");
+    };
 }
 
 // checks if pressure is 
@@ -57,6 +69,25 @@ fn is_below_isotherm_t_273_15(p: Pressure, h: AvailableEnergy) -> bool{
     let lower_bound_enthalpy = h_tp_eqm(lower_temp_bound, p);
 
     if h < lower_bound_enthalpy {
+        return true;
+    };
+
+    return false;
+
+}
+
+// making a function to check if p,h value is above the isotherm T = 1073.15K
+fn is_above_isotherm_t_1073_15(p: Pressure,h: AvailableEnergy) -> bool {
+    // first check if outside pressure range 
+    if is_outside_pressure_range(p) {
+        panic!("outside pressure range");
+    };
+
+    let upper_temp_bound = ThermodynamicTemperature::new::<kelvin>(1073.15);
+
+    let upper_bound_enthalpy = h_tp_eqm(upper_temp_bound, p);
+
+    if h > upper_bound_enthalpy {
         return true;
     };
 
