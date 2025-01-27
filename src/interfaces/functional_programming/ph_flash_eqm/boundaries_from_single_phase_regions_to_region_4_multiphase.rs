b86afee3d769f5ae3c00c139367de4dc4bdca7e8
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-27 09:50:32 +0800

    added boundary line for region 3 and 4

--- src/interfaces/functional_programming/ph_flash_eqm/boundaries_from_single_phase_regions_to_region_4_multiphase.rs
+++ src/interfaces/functional_programming/ph_flash_eqm/boundaries_from_single_phase_regions_to_region_4_multiphase.rs
@@ -5,12 +5,60 @@
 // equations:
 
 
+use crate::backward_eqn_ph_region_1_to_4::p_s3_h;
 use crate::region_1_subcooled_liquid::h_tp_1;
 use crate::region_2_vapour::h_tp_2;
 use crate::region_4_vap_liq_equilibrium::sat_temp_4;
 use crate::region_4_vap_liq_equilibrium::sat_pressure_4;
 use uom::si::f64::*;
+use uom::si::ratio::ratio;
 use uom::si::thermodynamic_temperature::kelvin;
+
+/// see page 38 
+pub(crate) fn is_ph_point_region_4_and_above_16_259_mpa(p: Pressure, h: AvailableEnergy) -> bool {
+
+    // before anything, check if enthalpy is within enthalpy validity range 
+    let ref_temperature = ThermodynamicTemperature::new::<kelvin>(623.15);
+    let ref_pressure = sat_pressure_4(ref_temperature);
+
+    let h_min_sat_liq = h_tp_1(ref_temperature, ref_pressure);
+    let h_max_sat_vap = h_tp_2(ref_temperature, ref_pressure);
+
+    if h > h_max_sat_vap {
+        panic!(" enthalpy of p,h point is outside validity range");
+    };
+    if h < h_min_sat_liq {
+        panic!(" enthalpy of p,h point is outside validity range");
+    };
+
+    // now, if within this range, we can check pressure
+    // in comparison to the saturation enthalpy
+    let mut p_sat_line = p_s3_h(h);
+
+
+
+    // if the h is very close to the two phase region 
+    // we need to correct this slightly
+    //
+    // This is in the section after table 2.30
+    if ((p - p_sat_line)/p).get::<ratio>() < 1e-4 {
+        p_sat_line = p_sat_line * (1.0 - 4.3e-6);
+    };
+    
+
+    // if pressure is greater than this pressure, then it is region 3 
+    // otherwise it's region 4 
+
+    if p < p_sat_line {
+
+        return true;
+
+    };
+
+    return false;
+
+}
+
 
 /// now, we are given (p,h) points,
 /// this enthalpy h can be greater or less than the saturated liquid 
