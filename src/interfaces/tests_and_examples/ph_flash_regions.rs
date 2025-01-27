202d14e105064ae009831b6ec88b034a3fb4102c
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-28 00:03:20 +0800

    added tests outside validity range region

--- src/interfaces/tests_and_examples/ph_flash_regions.rs
+++ src/interfaces/tests_and_examples/ph_flash_regions.rs
@@ -11,18 +11,6 @@ use crate::region_2_vapour::h_tp_2;
 use crate::region_3_single_phase_plus_supercritical_steam::{p_boundary_2_3, t_boundary_2_3};
 use crate::region_4_vap_liq_equilibrium::{sat_pressure_4, sat_temp_4};
 
-#[test] 
-pub fn test_pts_outside_validity_range(){
-
-    // this is just a reminder to test points 
-    // outside validity range of ph flash 
-    // eg region 5
-    //
-    // probably want to assert a failure or something
-    //
-    // so maybe i want to use an error or something
-    todo!();
-}
 
 
 /// below 16.529 MPa 
@@ -348,3 +336,171 @@ pub fn region_3_test_5(){
 
 
 }
+
+/// region 4
+///
+/// below 623.15 K VLE
+/// below 16.529 Mpa
+#[test]
+pub fn region_4_test_1(){
+
+    let reference_region = FwdEqnRegion::Region4;
+    let p = Pressure::new::<megapascal>(6.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1500.0);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// region 4
+///
+/// enthalpy at critical pt
+/// below 16.529 Mpa
+#[test]
+pub fn region_4_test_2(){
+
+    let reference_region = FwdEqnRegion::Region4;
+    let p = Pressure::new::<megapascal>(6.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+/// region 4
+///
+/// enthalpy just above TB23 line
+/// below 16.529 Mpa
+#[test]
+pub fn region_4_test_3(){
+
+    let reference_region = FwdEqnRegion::Region4;
+    let p = Pressure::new::<megapascal>(7.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2600.0);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// region 4
+///
+/// crit enthalpy
+/// pressure just below crit pressure
+#[test]
+pub fn region_4_test_4(){
+
+    let reference_region = FwdEqnRegion::Region4;
+    let p = Pressure::new::<megapascal>(22.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// region 5
+///
+/// should panic
+#[test]
+#[should_panic]
+pub fn region_5_test_1_should_panic(){
+
+    let reference_region = FwdEqnRegion::Region5;
+    let p = Pressure::new::<megapascal>(23.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(4300.0);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// above 100 mpa
+///
+/// should panic
+#[test]
+#[should_panic]
+pub fn above_100_mpa_should_panic(){
+
+    let reference_region = FwdEqnRegion::Region5;
+    let p = Pressure::new::<megapascal>(121.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(4300.0);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// enthalpy corresponding to temp below 273.15K 
+///
+/// should panic
+#[test]
+#[should_panic]
+pub fn above_enthalpy_below_273_15k_should_panic(){
+
+    let reference_region = FwdEqnRegion::Region5;
+    let p = Pressure::new::<megapascal>(121.0);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(-5.0);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
+/// pressure below 0.000611 MPa (triple pt pressure) should panic
+///
+/// should panic
+#[test]
+#[should_panic]
+pub fn above_pressure_below_tripe_pt_pressure_should_panic(){
+
+    let reference_region = FwdEqnRegion::Region5;
+    let p = Pressure::new::<megapascal>(6.11e-9);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+
+
+    let test_region = ph_flash_region(p, h);
+
+    assert_eq!(
+        reference_region,
+        test_region
+        );
+}
+
+
