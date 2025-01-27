2d1ec8690f2b00d6989873e0f77bfcbd55c8a10d
teddy0-mint-box <theodore_ong@berkeley.edu>
2025-01-27 10:13:00 +0800

    coded the boundary equations for the single phase regions

--- src/interfaces/functional_programming/ph_flash_eqm/boundaries_between_single_phase_regions.rs
+++ src/interfaces/functional_programming/ph_flash_eqm/boundaries_between_single_phase_regions.rs
@@ -1,6 +1,8 @@
 use crate::backward_eqn_ph_region_1_to_4::p_s3_h;
 use crate::region_1_subcooled_liquid::h_tp_1;
 use crate::region_2_vapour::h_tp_2;
+use crate::region_3_single_phase_plus_supercritical_steam::p_boundary_2_3;
+use crate::region_3_single_phase_plus_supercritical_steam::t_boundary_2_3;
 use crate::region_4_vap_liq_equilibrium::sat_temp_4;
 use crate::region_4_vap_liq_equilibrium::sat_pressure_4;
 use uom::si::f64::*;
@@ -32,6 +34,8 @@ pub(crate) fn is_ph_point_region_1_and_above_16_529_mpa(p: Pressure, h: Availabl
     let h_boundary_line = h_tp_1(ref_temperature, p);
 
     // if enthalpy is more than this boundary, then it's not region 1 
+    // i use greater than and NOT greater or equal to 
+    // because the boundary line belongs to reg 1
 
     if h > h_boundary_line {
         return false;
@@ -40,6 +44,48 @@ pub(crate) fn is_ph_point_region_1_and_above_16_529_mpa(p: Pressure, h: Availabl
     // else it's region 1
     //
 
+    return true;
+
+
+}
+
+/// also see page 39 
+pub(crate) fn is_ph_point_region_2_and_above_16_529_mpa(p: Pressure,
+    h: AvailableEnergy) -> bool {
+
+    // before anything, check if enthalpy is within enthalpy validity range 
+    let ref_temperature = ThermodynamicTemperature::new::<kelvin>(623.15);
+    let min_pressure = sat_pressure_4(ref_temperature);
+    let max_pressure = Pressure::new::<megapascal>(100.0);
+
+    if p < min_pressure {
+        panic!("p in (p,h) point is outside validity range");
+    };
+    if p > max_pressure {
+        panic!("p in (p,h) point is outside validity range");
+    };
+
+    // note that points along this boundary belongs to region 1 
+    // (see page 11)
+    // also, points along the P_B23 boundary line belong to region 2
+
+    // now let's get the boundary line enthalpy 
+    // first, get the appropriate temperature Tb23 
+    let t_boundary_b23 = t_boundary_2_3(p);
+
+    let h_boundary_line = h_tp_2(t_boundary_b23, p);
+
+    // if enthalpy is below this boundary line
+    //
+    // then its outside region 2
+    //
+    // i use smaller than and NOT smaller or equal to 
+    // because the boundary line belongs to reg 2
+    if h < h_boundary_line {
+        return false;
+    };
+
+    // otherwise it's inside region 2
     return true;
 
 
