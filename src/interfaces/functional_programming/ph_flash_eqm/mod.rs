cabf04cc28318c3e8ca6566f72c574ed10293d4b
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-01-29 20:11:06 +0800

    volume equations working well-ish, changed the flashing equations

--- src/interfaces/functional_programming/ph_flash_eqm/mod.rs
+++ src/interfaces/functional_programming/ph_flash_eqm/mod.rs
@@ -1,6 +1,6 @@
 use uom::si::{available_energy::kilojoule_per_kilogram, f64::*, pressure::megapascal, ratio::ratio, thermodynamic_temperature::kelvin};
 
-use crate::{region_1_subcooled_liquid::{alpha_v_tp_1, cp_tp_1, cv_tp_1, h_tp_1, kappa_t_tp_1, kappa_tp_1, s_tp_1, t_ph_1, u_tp_1, v_tp_1, w_tp_1, InversePressure}, region_2_vapour::{alpha_v_tp_2, cp_tp_2, cv_tp_2, h_tp_2, kappa_t_tp_2, kappa_tp_2, s_tp_2, t_ph_2, u_tp_2, v_tp_2, w_tp_2}, region_3_single_phase_plus_supercritical_steam::{alpha_v_tp_3, cp_tp_3, cv_tp_3, kappa_t_tp_3, kappa_tp_3, s_rho_t_3, s_tp_3, t_ph_3, u_rho_t_3, u_tp_3, v_ph_3, v_tp_3, v_tp_3c, v_tp_3r, v_tp_3s, v_tp_3t, v_tp_3u, v_tp_3x, v_tp_3y, v_tp_3z, w_tp_3}, region_4_vap_liq_equilibrium::{sat_pressure_4, sat_temp_4}, region_5_superheated_steam::{alpha_v_tp_5, cp_tp_5, cv_tp_5, kappa_t_tp_5, kappa_tp_5, s_tp_5, u_tp_5, w_tp_5}};
+use crate::{region_1_subcooled_liquid::{alpha_v_tp_1, cp_tp_1, cv_tp_1, h_tp_1, kappa_t_tp_1, kappa_tp_1, s_tp_1, t_ph_1, u_tp_1, v_tp_1, w_tp_1, InversePressure}, region_2_vapour::{alpha_v_tp_2, cp_tp_2, cv_tp_2, h_tp_2, kappa_t_tp_2, kappa_tp_2, s_tp_2, t_ph_2, u_tp_2, v_tp_2, w_tp_2}, region_3_single_phase_plus_supercritical_steam::{alpha_v_tp_3, cp_tp_3, cv_tp_3, h_rho_t_3, kappa_t_tp_3, kappa_tp_3, s_rho_t_3, s_tp_3, t_ph_3, u_rho_t_3, u_tp_3, v_ph_3, v_ph_3a, v_tp_3, v_tp_3c, v_tp_3r, v_tp_3s, v_tp_3t, v_tp_3u, v_tp_3x, v_tp_3y, v_tp_3z, w_tp_3}, region_4_vap_liq_equilibrium::{sat_pressure_4, sat_temp_4}, region_5_superheated_steam::{alpha_v_tp_5, cp_tp_5, cv_tp_5, kappa_t_tp_5, kappa_tp_5, s_tp_5, u_tp_5, w_tp_5}};
 
 use super::pt_flash_eqm::FwdEqnRegion;
 
@@ -448,12 +448,71 @@ pub fn x_ph_flash(p: Pressure, h: AvailableEnergy,) -> f64 {
             // first let's take saturation pressure 
             let t_sat = sat_temp_4(p);
 
-            let h_liq = h_tp_1(t_sat, p);
-            let h_vap = h_tp_2(t_sat, p);
 
-            let x: Ratio = (h-h_liq)/(h_vap-h_liq);
+            let t_sat_kelvin = t_sat.get::<kelvin>();
+            if t_sat_kelvin <= 623.15 {
+                let h_liq = h_tp_1(t_sat, p);
+                let h_vap = h_tp_2(t_sat, p);
 
-            return x.get::<ratio>();
+                let x: Ratio = (h-h_liq)/(h_vap-h_liq);
+                return x.get::<ratio>();
+            } else {
+                // in the case we hit region 3 and region 4 boundary, the algorithm must differ
+
+                // looks like all I need to do is use the backward equations 
+                // v(t,p)
+                // let's do the liquid one first 
+
+                // this is with reference to the pt equations on 
+                // fig 2.24 page 109
+                //
+                // alternatively, this is a cheat way... 
+                // I look at t_sat, and force it to find liquid volume 
+                // using a slightly colder temperature than tsat 
+                // and for vapour I get it slightly higher than  
+                // the tsat
+                let p_mpa = p.get::<megapascal>();
+                let v_vap: SpecificVolume = {
+                    // this covers up to tsat at 643.15 K
+                    if t_sat_kelvin <= 640.691 {
+                        v_tp_3t(t_sat, p)
+                    } else if t_sat_kelvin <= 643.15 {
+                        v_tp_3r(t_sat, p)
+                    } else // this covers pressure from 21.0434 Mpa to crit point 
+                    if p_mpa <= 21.9010 {
+                        v_tp_3x(t_sat, p)
+                    } else {
+                        v_tp_3z(t_sat, p)
+                    }
+                };
+
+                let v_liq: SpecificVolume = {
+                    // this covers up to tsat at 643.15 K
+                    if t_sat_kelvin <= 634.659 {
+                        v_tp_3c(t_sat, p)
+                    } else if t_sat_kelvin <= 643.15 {
+                        v_tp_3s(t_sat, p)
+                    } else if p_mpa <= 21.9316 {
+                        v_tp_3u(t_sat, p)
+                    } else {
+                        v_tp_3y(t_sat, p)
+                    }
+
+
+                };
+                // now we have t v equations, we can get h_liq 
+                // and h_vap
+                // using t,v equations
+
+                let h_liq = h_rho_t_3(v_liq.recip(), t_sat);
+                let h_vap = h_rho_t_3(v_vap.recip(), t_sat);
+
+
+                let x: Ratio = (h-h_liq)/(h_vap-h_liq);
+                return x.get::<ratio>();
+
+
+            }
 
         },
         // this is a placeholder, 
