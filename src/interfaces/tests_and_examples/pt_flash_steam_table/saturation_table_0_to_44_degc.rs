d2b33cce06f2eae128c686d3c50dbd35daf1b60e
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-02-04 13:27:59 +0800

    tp flash tested for 0 to 44 degc region 4

--- src/interfaces/tests_and_examples/pt_flash_steam_table/saturation_table_0_to_44_degc.rs
+++ src/interfaces/tests_and_examples/pt_flash_steam_table/saturation_table_0_to_44_degc.rs
@@ -6,12 +6,12 @@ use uom::si::specific_volume::cubic_meter_per_kilogram;
 use uom::si::thermodynamic_temperature::{degree_celsius, kelvin};
 
 use crate::interfaces::functional_programming::ph_flash_eqm::{s_ph_eqm, t_ph_eqm, v_ph_eqm};
+use crate::interfaces::functional_programming::pt_flash_eqm::{h_tp_eqm_two_phase, s_tp_eqm_two_phase, v_tp_eqm_two_phase};
 use crate::region_1_subcooled_liquid::h_tp_1;
 use crate::region_2_vapour::h_tp_2;
 
 /// saturation table (see page 174)
 #[test]
-#[ignore = "yet to implement proper test"]
 pub fn saturation_table_0_to_44_degc(){
 
     //[t_deg_c,t_kelvin,psat_bar,v_liq_m3_per_kg,v_vap_m3_per_kg,h_liq_kj_per_kg,h_vap_kj_per_kg,enthalpy_of_vap,s_liq_kj_per_kg_k,s_vap_kj_per_kg_k],
@@ -82,12 +82,11 @@ pub fn saturation_table_0_to_44_degc(){
                 s_liq_kj_per_kg_k, s_vap_kj_per_kg_k);
         }
 
-        todo!()
 
 }
 
 fn assert_pt_flash(t_deg_c: f64,
-    t_kelvin: f64,
+    _t_kelvin: f64,
     psat_bar: f64,
     v_liq_m3_per_kg: f64,
     v_vap_m3_per_kg: f64,
@@ -97,7 +96,58 @@ fn assert_pt_flash(t_deg_c: f64,
     s_liq_kj_per_kg_k: f64,
     s_vap_kj_per_kg_k: f64){
 
+    // specify a vapour quality
+    let x_ref = 0.7;
+    let p = Pressure::new::<bar>(psat_bar);
+    let t = ThermodynamicTemperature::new::<degree_celsius>(t_deg_c);
 
+    // first test enthalpy 
+    let x = x_ref;
+    let h_test = h_tp_eqm_two_phase(t, p, x);
+    let h_ref = x_ref * h_vap_kj_per_kg + (1.0-x_ref) * h_liq_kj_per_kg;
+
+    approx::assert_relative_eq!(
+        h_ref,
+        h_test.get::<kilojoule_per_kilogram>(),
+        max_relative=1e-5
+        );
+
+    // then liquid and vapour specific vol
+    let v_ref_m3_per_kg = (1.0 - x_ref) * v_liq_m3_per_kg + x_ref * v_vap_m3_per_kg;
+    let v = v_tp_eqm_two_phase(t, p, x);
+
+    approx::assert_relative_eq!(
+        v_ref_m3_per_kg,
+        v.get::<cubic_meter_per_kilogram>(),
+        max_relative=1e-5
+        );
+
+    // liquid and vapour s 
+    let s_ref_kj_per_kg_k = 
+        (1.0 - x_ref) * s_liq_kj_per_kg_k 
+        + x_ref * s_vap_kj_per_kg_k;
+
+    let s = s_tp_eqm_two_phase(t, p, x);
+
+    approx::assert_relative_eq!(
+        s_ref_kj_per_kg_k,
+        s.get::<kilojoule_per_kilogram_kelvin>(),
+        max_relative=1e-5
+        );
+
+
+    // enthalpy of vaporisation
+    // a.k.a latent heat
+    // kind of manual, not really in the flashing function
+    // per se, but it works!
+    let enthalpy_of_vap = h_tp_eqm_two_phase(t, p, 1.0)
+        - h_tp_eqm_two_phase(t, p, 0.0);
+
+    approx::assert_relative_eq!(
+        enthalpy_of_vap_kj_per_kg,
+        enthalpy_of_vap.get::<kilojoule_per_kilogram>(),
+        max_relative=1e-5
+        );
 
 
 
