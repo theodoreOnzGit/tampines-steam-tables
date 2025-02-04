cc1dbc0649145c566e77e73c0cf48b2c229ebbf9
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-02-04 18:22:33 +0800

    changed the tests that need to be done... to mark that ph flash is completed, except for the thermal conductivity and viscosity

--- src/interfaces/tests_and_examples/mod.rs
+++ src/interfaces/tests_and_examples/mod.rs
@@ -1,10 +1,6 @@
 /// basically this is a placeholder to 
 #[test]
-pub fn note_to_self_do_steam_table_pt_flash(){
-    todo!()
-}
-#[test]
-pub fn note_to_self_do_steam_table_ph_flash(){
+pub fn note_to_self_do_steam_table_pt_flash_for_high_temp_region(){
     todo!()
 }
 
