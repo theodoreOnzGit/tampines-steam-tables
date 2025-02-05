f3477651a04191456571e60e2453cfab5cbbf3a1
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-02-06 02:22:42 +0800

    testing psi 1 for viscosity, still needs work

--- src/dynamic_viscosity/mod.rs
+++ src/dynamic_viscosity/mod.rs
@@ -1,13 +1,37 @@
 use uom::si::{f64::*, ratio::ratio};
 
-use crate::constants::t_crit_water;
+use crate::constants::{rho_crit_water, t_crit_water};
 
 
-pub const PSI_0_COEFFS: [[f64; 2]; 4] = [
+const PSI_0_COEFFS: [[f64; 2]; 4] = [
     [1.0,  0.167_752e-1],
     [2.0,  0.220_462e-1],
     [3.0,  0.636_656_4e-2],
     [4.0,  -0.241_605e-2],
+];
+
+const PSI_1_COEFFS: [[f64; 4]; 21] = [
+    [1.0, 0.0, 0.0, 0.529_094],
+    [2.0, 0.0, 1.0, 0.850_895e-1],
+    [3.0, 0.0, 2.0, -0.108_374e1],
+    [4.0, 0.0, 3.0, -0.289_555],
+    [5.0, 1.0, 0.0, 0.222_531],
+    [6.0, 1.0, 1.0, 0.999_115],
+    [7.0, 1.0, 2.0, 0.188_797e1],
+    [8.0, 1.0, 3.0, 0.126_613e1],
+    [9.0, 1.0, 5.0, 0.120_573],
+    [10.0, 2.0, 0.0, -0.281_378],
+    [11.0, 2.0, 1.0, -0.906_851],
+    [12.0, 2.0, 2.0, -0.772_479],
+    [13.0, 2.0, 3.0, -0.489_837],
+    [14.0, 2.0, 4.0, -0.257_040],
+    [15.0, 3.0, 0.0, 0.161_913],
+    [16.0, 3.0, 1.0, 0.257_399],
+    [17.0, 4.0, 0.0, -0.325_372e-1],
+    [18.0, 4.0, 3.0, 0.698_452e-1],
+    [19.0, 5.0, 4.0, 0.872_102e-2],
+    [20.0, 6.0, 3.0, -0.435_673e-2],
+    [21.0, 6.0, 5.0, -0.593_264e-3],
 ];
 
 pub(crate) fn psi_0_viscosity(t: ThermodynamicTemperature) -> f64 {
@@ -26,6 +50,35 @@ pub(crate) fn psi_0_viscosity(t: ThermodynamicTemperature) -> f64 {
     }
 
     return theta_f64.sqrt()*den.recip();
+
+
+}
+pub(crate) fn psi_1_viscosity(
+    t: ThermodynamicTemperature,
+    rho: MassDensity) -> f64 {
+    let t_crit_water = t_crit_water();
+    let theta = t/t_crit_water;
+    let theta_f64 = theta.get::<ratio>();
+
+    let rho_crit_water = rho_crit_water();
+    let delta = rho/rho_crit_water;
+    let delta_f64 = delta.get::<ratio>();
+
+    let mut exponent: f64 = 0.0;
+
+    for coeffs in PSI_1_COEFFS {
+        let _i = coeffs[0];
+        let ii = coeffs[1];
+        let ji = coeffs[2];
+        let ni = coeffs[3];
+
+        exponent += ni * (delta_f64 - 1.0).powf(ii) * (theta_f64.recip() - 1.0).powf(ji);
+
+    }
+
+    exponent *= delta_f64;
+
+    return exponent.exp();
 
 
 }
--- src/dynamic_viscosity/tests.rs
+++ src/dynamic_viscosity/tests.rs
@@ -1,4 +1,6 @@
-use uom::si::{f64::*, thermodynamic_temperature::kelvin};
+use uom::si::{f64::*, mass_density::kilogram_per_cubic_meter, thermodynamic_temperature::kelvin};
+
+use crate::dynamic_viscosity::psi_1_viscosity;
 
 use super::psi_0_viscosity;
 #[test]
@@ -34,6 +36,51 @@ pub fn psi_0_viscosity_test(){
     let psi_test = psi_0_viscosity(t4);
     approx::assert_relative_eq!(
         psi_0_4,
+        psi_test,
+        max_relative=1e-8
+        );
+
+}
+
+
+#[test]
+pub fn psi_1_viscosity_test(){
+    let t1 = ThermodynamicTemperature::new::<kelvin>(298.15);
+    let t2 = ThermodynamicTemperature::new::<kelvin>(873.15);
+    let t3 = ThermodynamicTemperature::new::<kelvin>(673.15);
+    let t4 = ThermodynamicTemperature::new::<kelvin>(1173.15);
+
+    let rho1 = MassDensity::new::<kilogram_per_cubic_meter>(0.997_047_435e3);
+    let rho2 = MassDensity::new::<kilogram_per_cubic_meter>(0.549_921_814e3);
+    let rho3 = MassDensity::new::<kilogram_per_cubic_meter>(0.612_391_201e3);
+    let rho4 = MassDensity::new::<kilogram_per_cubic_meter>(0.377_584_847e2);
+
+    let psi_1_1 = 0.916_694_207e2;
+    let psi_1_2 = 0.104_200_938e1;
+    let psi_1_3 = 0.296_900_349e1;
+    let psi_1_4 = 0.102_422_324e1;
+
+    let psi_test = psi_1_viscosity(t1, rho1);
+    approx::assert_relative_eq!(
+        psi_1_1,
+        psi_test,
+        max_relative=1e-8
+        );
+    let psi_test = psi_0_viscosity(t2);
+    approx::assert_relative_eq!(
+        psi_1_2,
+        psi_test,
+        max_relative=1e-8
+        );
+    let psi_test = psi_0_viscosity(t3);
+    approx::assert_relative_eq!(
+        psi_1_3,
+        psi_test,
+        max_relative=1e-8
+        );
+    let psi_test = psi_0_viscosity(t4);
+    approx::assert_relative_eq!(
+        psi_1_4,
         psi_test,
         max_relative=1e-8
         );
