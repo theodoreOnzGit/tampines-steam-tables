e51d061378794d44f439a4d8e4c59106a2e01fc5
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2025-02-10 10:08:59 +0800

    thermal conductivity c tested for a few regimes

--- src/thermal_conductivity/tests.rs
+++ src/thermal_conductivity/tests.rs
@@ -85,3 +85,53 @@ pub fn lambda_1_test(){
         );
     
 }
+
+#[test]
+fn c_test(){
+    let _t1 = ThermodynamicTemperature::new::<kelvin>(298.15);
+    let _t2 = ThermodynamicTemperature::new::<kelvin>(873.15);
+    let _t3 = ThermodynamicTemperature::new::<kelvin>(673.15);
+    let _t4 = ThermodynamicTemperature::new::<kelvin>(1173.15);
+
+    let rho1 = MassDensity::new::<kilogram_per_cubic_meter>(0.997_047_435e3);
+    let rho2 = MassDensity::new::<kilogram_per_cubic_meter>(0.260_569_558e2);
+    let rho3 = MassDensity::new::<kilogram_per_cubic_meter>(0.523_371_289e3);
+    let rho4 = MassDensity::new::<kilogram_per_cubic_meter>(0.377_584_848e2);
+
+    let delta_1: f64 = (rho1/rho_crit_water()).get::<ratio>();
+    let delta_2: f64 = (rho2/rho_crit_water()).get::<ratio>();
+    let delta_3: f64 = (rho3/rho_crit_water()).get::<ratio>();
+    let delta_4: f64 = (rho4/rho_crit_water()).get::<ratio>();
+
+    let c_1 = 0.129_592_952e-1;
+    let c_2 = 0.163_793_337e0;
+    let c_3 = 0.940_881_573e-1;
+    let c_4 = 0.168_780_175e0;
+
+    dbg!(&(delta_1,delta_2,delta_3,delta_4));
+    let c_test = captial_c(delta_1);
+    approx::assert_relative_eq!(
+        c_1,
+        c_test,
+        max_relative=1e-8
+        );
+    let c_test = captial_c(delta_2);
+    approx::assert_relative_eq!(
+        c_2,
+        c_test,
+        max_relative=1e-8
+        );
+    let c_test = captial_c(delta_3);
+    approx::assert_relative_eq!(
+        c_3,
+        c_test,
+        max_relative=1e-8
+        );
+    let c_test = captial_c(delta_4);
+    approx::assert_relative_eq!(
+        c_4,
+        c_test,
+        max_relative=1e-8
+        );
+    
+}
