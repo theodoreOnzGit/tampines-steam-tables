5704410115bc5ecabe74d4e5d7c7aa481531945e
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-02-11 21:49:08 +0800

    subregion 2c equations tested successfully

--- src/region_2_vapour/backward_eqn_ps_2/mod.rs
+++ src/region_2_vapour/backward_eqn_ps_2/mod.rs
@@ -1,2 +1,3 @@
 pub mod subregion_2a;
 pub mod subregion_2b;
+pub mod subregion_2c;
--- src/region_2_vapour/tests/region_2_t_ps_flash/mod.rs
+++ src/region_2_vapour/tests/region_2_t_ps_flash/mod.rs
@@ -6,6 +6,7 @@ use uom::si::thermodynamic_temperature::kelvin;
 
 use crate::region_2_vapour::subregion_2a::t_ps_2a;
 use crate::region_2_vapour::subregion_2b::t_ps_2b;
+use crate::region_2_vapour::subregion_2c::t_ps_2c;
 
 #[test]
 pub fn subregion_2a_t_ps_test_1(){
@@ -97,6 +98,56 @@ pub fn subregion_2b_t_ps_test_3(){
     let t_expected_kelvin = 0.103_801_126e4;
 
     let t_test = t_ps_2b(p, s);
+
+    approx::assert_relative_eq!(
+        t_expected_kelvin,
+        t_test.get::<kelvin>(),
+        max_relative=1e-8
+        );
+
+}
+
+
+#[test]
+pub fn subregion_2c_t_ps_test_1(){
+    let p = Pressure::new::<megapascal>(20.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.75);
+    let t_expected_kelvin = 0.697_992_849e3;
+
+    let t_test = t_ps_2c(p, s);
+
+    approx::assert_relative_eq!(
+        t_expected_kelvin,
+        t_test.get::<kelvin>(),
+        max_relative=1e-8
+        );
+
+}
+
+
+#[test]
+pub fn subregion_2c_t_ps_test_2(){
+    let p = Pressure::new::<megapascal>(80.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.25);
+    let t_expected_kelvin = 0.854_011_484e3;
+
+    let t_test = t_ps_2c(p, s);
+
+    approx::assert_relative_eq!(
+        t_expected_kelvin,
+        t_test.get::<kelvin>(),
+        max_relative=1e-8
+        );
+
+}
+
+#[test]
+pub fn subregion_2c_t_ps_test_3(){
+    let p = Pressure::new::<megapascal>(80.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.75);
+    let t_expected_kelvin = 0.949_017_998e3;
+
+    let t_test = t_ps_2c(p, s);
 
     approx::assert_relative_eq!(
         t_expected_kelvin,
