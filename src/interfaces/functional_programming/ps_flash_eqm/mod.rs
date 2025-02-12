81809897cf0cbd7f61c3d1be704be4667aa945b6
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-02-12 17:27:10 +0800

    modified ph boundary checks to ps boundary checks

---
+++ src/interfaces/functional_programming/ps_flash_eqm/mod.rs
@@ -0,0 +1,24 @@
+/// checks if (p,s) point is within validity range
+/// of region 1 to 4
+pub(crate) mod validity_range;
+pub(crate) use validity_range::*;
+
+/// this checks for boundary between single phase regions (1 to 3) 
+/// and multiphase region 4
+pub(crate) mod boundaries_from_single_phase_regions_to_region_4_multiphase;
+pub(crate) use boundaries_from_single_phase_regions_to_region_4_multiphase::*;
+
+use uom::si::f64::*;
+
+fn check_if_within_ps_validity_region(p: Pressure, s: SpecificHeatCapacity,){
+    if is_outside_pressure_range(p) {
+        panic!("p,s point is outside pressure range");
+    };
+
+    if is_below_isotherm_t_273_15(p, s) {
+        panic!("p,s point below 273.15K");
+    };
+    if is_above_isotherm_t_1073_15(p, s) {
+        panic!("p,s point above 1073.15K");
+    };
+}
---
+++ src/interfaces/functional_programming/ps_flash_eqm/validity_range.rs
@@ -0,0 +1,75 @@
+use uom::si::{f64::*, pressure::megapascal, thermodynamic_temperature::kelvin};
+
+use crate::{interfaces::functional_programming::pt_flash_eqm::s_tp_eqm_single_phase, region_4_vap_liq_equilibrium::sat_pressure_4};
+
+// checks if pressure is 
+// lower than saturation pressure at 273.15K or higher than 100 MPa
+//
+// if so, it falls outside the ps boundary
+pub(crate) fn is_outside_pressure_range(p: Pressure) -> bool {
+
+    // first determine if p,h point is outside pressure range
+    let lower_pressure_limit: Pressure = 
+        sat_pressure_4(
+            ThermodynamicTemperature::new::<kelvin>(273.15)
+        );
+
+    let upper_pressure_limit: Pressure = Pressure::new::<megapascal>(100.0);
+
+    if p < lower_pressure_limit {
+        return true;
+    };
+
+    if p > upper_pressure_limit {
+        return true;
+    };
+
+    return false;
+
+}
+
+// making a function to check if a p,s value is below the isotherm at 
+// 273.15K
+//
+pub(crate) fn is_below_isotherm_t_273_15(p: Pressure, s: SpecificHeatCapacity) -> bool{
+
+    // first check if outside pressure range 
+    if is_outside_pressure_range(p) {
+        panic!("outside pressure range");
+    };
+
+    // let's have the lower enthalpy range 
+    let lower_temp_bound = ThermodynamicTemperature::new::<kelvin>(273.15);
+
+    let lower_bound_entropy = s_tp_eqm_single_phase(lower_temp_bound, p);
+
+    if s < lower_bound_entropy {
+        return true;
+    };
+
+    return false;
+
+}
+
+// making a function to check if p,h value is above the isotherm T = 1073.15K
+pub(crate) fn is_above_isotherm_t_1073_15(p: Pressure,s: SpecificHeatCapacity) -> bool {
+    // first check if outside pressure range 
+    if is_outside_pressure_range(p) {
+        panic!("outside pressure range");
+    };
+
+    let upper_temp_bound = ThermodynamicTemperature::new::<kelvin>(1073.15);
+
+    let upper_bound_enthalpy = s_tp_eqm_single_phase(upper_temp_bound, p);
+
+    if s > upper_bound_enthalpy {
+        return true;
+    };
+
+    return false;
+
+}
+
+
+
+
