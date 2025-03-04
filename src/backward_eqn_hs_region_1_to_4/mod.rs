32260a978531a85c5ced9679b9c6fb32c6448e19
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-03-04 17:10:51 +0800

    boundary eqn b13 test completed

--- src/backward_eqn_hs_region_1_to_4/mod.rs
+++ src/backward_eqn_hs_region_1_to_4/mod.rs
@@ -3,3 +3,4 @@ mod tests;
 
 pub mod saturated_liquid_line;
 pub mod saturated_vapour_line;
+pub mod region_1_and_3;
--- src/backward_eqn_hs_region_1_to_4/tests/mod.rs
+++ src/backward_eqn_hs_region_1_to_4/tests/mod.rs
@@ -5,3 +5,8 @@ pub mod saturated_liquid_line;
 /// uses table 2.72 in steam tables book 
 /// along with other tables
 pub mod saturated_vapour_line;
+
+/// uses table 2.73 in steam tables book 
+/// along with other tables
+pub mod region_1_and_3;
+
