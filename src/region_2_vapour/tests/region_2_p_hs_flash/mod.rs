c14312a25fc41cee8158411a1edd8d801a4a4696
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-03-05 10:49:38 +0800

    added tests for p2b and p2c for hs flash, pending input of coeffs

--- src/region_2_vapour/tests/region_2_p_hs_flash/mod.rs
+++ src/region_2_vapour/tests/region_2_p_hs_flash/mod.rs
@@ -4,7 +4,7 @@ use uom::si::f64::*;
 use uom::si::quantities::SpecificHeatCapacity;
 use uom::si::specific_heat_capacity::kilojoule_per_kilogram_kelvin;
 
-use crate::region_2_vapour::p_hs_2a;
+use crate::region_2_vapour::{p_hs_2a, p_hs_2b, p_hs_2c};
 
 #[test]
 pub fn subregion_2a_p_hs_test_1(){
@@ -43,6 +43,100 @@ pub fn subregion_2a_p_hs_test_3(){
     let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(9.5);
 
     let p_test = p_hs_2a(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+
+
+#[test]
+pub fn subregion_2b_p_hs_test_1(){
+    let p = Pressure::new::<megapascal>(4.793_911_442);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2800.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(6.0);
+
+    let p_test = p_hs_2b(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+#[test]
+pub fn subregion_2b_p_hs_test_2(){
+    let p = Pressure::new::<megapascal>(8.395_519_209e1);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3600.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(6.0);
+
+    let p_test = p_hs_2b(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+#[test]
+pub fn subregion_2b_p_hs_test_3(){
+    let p = Pressure::new::<megapascal>(7.527_161_441);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3600.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(7.0);
+
+    let p_test = p_hs_2b(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+
+
+#[test]
+pub fn subregion_2c_p_hs_test_1(){
+    let p = Pressure::new::<megapascal>(9.439_202_060e1);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2800.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.1);
+
+    let p_test = p_hs_2c(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+#[test]
+pub fn subregion_2c_p_hs_test_2(){
+    let p = Pressure::new::<megapascal>(8.414_574_124);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2800.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.8);
+
+    let p_test = p_hs_2c(h, s);
+
+    approx::assert_relative_eq!(
+        p.get::<megapascal>(),
+        p_test.get::<megapascal>(),
+        max_relative=1e-8
+        );
+
+}
+#[test]
+pub fn subregion_2c_p_hs_test_3(){
+    let p = Pressure::new::<megapascal>(8.376_903_879e1);
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(3400.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.8);
+
+    let p_test = p_hs_2c(h, s);
 
     approx::assert_relative_eq!(
         p.get::<megapascal>(),
