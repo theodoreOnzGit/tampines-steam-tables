78c3f4f30104cf1dd92d6e7b9365e1d4852a76f3
guest at arch-aftershock-home <theodore_ong@berkeley.edu>
2025-03-31 20:16:02 +0800

    completed roughly region 1 and region 4 tests

--- src/interfaces/functional_programming/hs_flash_eqm/region_split_tests.rs
+++ src/interfaces/functional_programming/hs_flash_eqm/region_split_tests.rs
@@ -13,3 +13,100 @@ fn region1_test_1(){
     assert_eq!(correct_region, test_region);
     
 }
+
+
+#[test]
+fn region1_test_2(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1550.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(3.398);
+
+    let correct_region = BackwdEqnSubRegion::Region1;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+
+
+#[test]
+fn region4_test_1(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1000.0);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(3.398);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+#[test]
+fn region4_test_2(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1553.9);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(3.778);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+// near critical entropy
+#[test]
+fn region4_test_3(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(1553.9);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(4.412);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+// near critical entropy
+#[test]
+fn region4_test_4(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(5.211);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+// near critical entropy
+#[test]
+fn region4_test_5(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(6.040);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
+
+
+// near critical entropy
+#[test]
+fn region4_test_6(){
+    let h = AvailableEnergy::new::<kilojoule_per_kilogram>(2087.5);
+    let s = SpecificHeatCapacity::new::<kilojoule_per_kilogram_kelvin>(8.0);
+
+    let correct_region = BackwdEqnSubRegion::Region4;
+    let test_region = hs_flash_region(h, s);
+
+    assert_eq!(correct_region, test_region);
+    
+}
