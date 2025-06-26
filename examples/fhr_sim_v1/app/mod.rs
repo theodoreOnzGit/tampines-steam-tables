3f428e5ba3d9e04f162c7018d07481bbd6835089
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2025-06-26 20:06:22 +0800

    integrated fhr simulator v1 into tampines-steam-tables

---
+++ examples/fhr_sim_v1/app/mod.rs
@@ -0,0 +1,289 @@
+use std::time::Duration;
+
+use egui::{vec2, Pos2, Rect};
+use local_widgets_and_buttons::{fhr_reactor_widget::FHRReactorWidget, pipes::SinglePipe};
+use uom::si::f64::*;
+use uom::si::thermodynamic_temperature::degree_celsius;
+
+use crate::{FHRSimulatorApp, FHRState};
+use crate::Panel;
+
+pub mod prke_backend;
+pub mod thermal_hydraulics_backend;
+
+impl eframe::App for FHRSimulatorApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.heading("FHR Educational Simulator v1");
+            ui.separator();
+            egui::menu::bar(ui, |ui| {
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+            // allow user to select which panel is open
+            ui.horizontal( 
+                |ui| {
+                    ui.selectable_value(&mut self.open_panel, Panel::MainPage, "Main Page"); 
+                    ui.selectable_value(&mut self.open_panel, Panel::ReactorPowerGraphs, "Power Diagnostics"); 
+                    ui.selectable_value(&mut self.open_panel, Panel::PoisonGraphs, "Reactor Poison Diagnostics"); 
+            }
+            );
+            ui.separator();
+        });
+
+        egui::SidePanel::right("Supplementary Info").show(ctx, |ui|{
+            self.side_panel(ui);
+
+
+
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            ui.separator();
+            egui::ScrollArea::both()
+                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::AlwaysVisible)
+                .drag_to_scroll(true)
+                .show(ui, |ui| {
+
+
+                    match self.open_panel {
+                        Panel::MainPage => self.main_page(ui),
+                        Panel::ReactorPowerGraphs => self.reactor_power_page_graph(ui),
+                        Panel::PoisonGraphs => {},
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+        });
+
+        egui::TopBottomPanel::bottom("github").show(ctx, |ui|{
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+
+        });
+
+        
+
+
+        ctx.request_repaint_after(Duration::from_millis(50));
+
+        // adding the return here because there are too many closing 
+        // parantheses
+        // just demarcates the end
+        return ();
+    }
+
+}
+
+impl FHRSimulatorApp {
+    pub fn main_page(&mut self, ui: &mut egui::Ui,){
+
+        // for painting widgets
+        // https://github.com/emilk/egui/blob/master/crates/egui_demo_lib/src/demo/misc_demo_window.rs
+        //
+        // the main thing is the painter class:
+        // https://docs.rs/egui/latest/egui/struct.Painter.html
+        //
+        // here you can paint circles and rectangles 
+        // images, line segments etc.
+        // obtain lock first 
+
+        // quickly clone the fhr state and drop ptr asap 
+        // just to read
+        let fhr_state_ptr = self.fhr_state.lock().unwrap();
+        let fhr_state_clone: FHRState = fhr_state_ptr.clone();
+        drop(fhr_state_ptr);
+
+        let left_control_rod_insertion_frac 
+            = fhr_state_clone.left_cr_insertion_frac;
+        let right_control_rod_insertion_frac 
+            = fhr_state_clone.right_cr_insertion_frac;
+
+
+
+        let ui_rectangle: Rect = ui.min_rect();
+
+        // this gives coordinates of top and left of the ui
+        // for relative placement
+        let left_most_side = ui_rectangle.left();
+        let top_most_side = ui_rectangle.top();
+
+        let reactor_offset_x: f32 = 100.0;
+        let reactor_offset_y: f32 = 200.0;
+        let reactor_x_width_px: f32 = 150.0 * 1.5;
+        let reactor_y_height_px: f32 = 700.0 * 1.5;
+
+
+        let reactor_rect_top_left: Pos2 = 
+            Pos2 { 
+                x: left_most_side + reactor_offset_x, 
+                y: top_most_side + reactor_offset_y
+            };
+        let reactor_rect_bottom_right: Pos2 = 
+            Pos2 { 
+                x: reactor_rect_top_left.x + reactor_x_width_px, 
+                y: reactor_rect_top_left.y + reactor_y_height_px
+            };
+        let reactor_rectangle: egui::Rect =
+            egui::Rect{
+                min: reactor_rect_top_left,
+                max: reactor_rect_bottom_right,
+            };
+
+
+        let fhr_size = 
+            vec2(reactor_rectangle.width(), reactor_rectangle.height());
+
+        let min_temp = ThermodynamicTemperature::new::<degree_celsius>(450.0);
+        let max_temp = ThermodynamicTemperature::new::<degree_celsius>(1000.0);
+        let pebble_core_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.pebble_core_temp_degc
+        );
+        let pebble_bed_coolant_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.pebble_bed_coolant_temp_degc
+        );
+        let core_bottom_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.core_bottom_temp_degc
+        );
+        let core_top_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.core_top_temp_degc
+        );
+        let core_inlet_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.core_inlet_temp_degc
+        );
+        let core_outlet_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.core_outlet_temp_degc
+        );
+        let left_downcomer_upper_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.left_downcomer_upper_temp_degc
+        );
+        let left_downcomer_mid_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.left_downcomer_mid_temp_degc
+        );
+        let left_downcomer_lower_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.left_downcomer_lower_temp_degc
+        );
+        let right_downcomer_upper_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.right_downcomer_upper_temp_degc
+        );
+        let right_downcomer_mid_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.right_downcomer_mid_temp_degc
+        );
+        let right_downcomer_lower_temp = ThermodynamicTemperature::new::<degree_celsius>(
+            fhr_state_clone.right_downcomer_lower_temp_degc
+        );
+
+
+        let mut fhr_widget = FHRReactorWidget::new(
+            fhr_size,
+            min_temp,
+            max_temp,
+            pebble_core_temp,
+            pebble_bed_coolant_temp,
+            core_bottom_temp,
+            core_top_temp,
+            core_inlet_temp,
+            core_outlet_temp,
+            left_downcomer_upper_temp,
+            left_downcomer_mid_temp,
+            left_downcomer_lower_temp,
+            right_downcomer_upper_temp,
+            right_downcomer_mid_temp,
+            right_downcomer_lower_temp,
+        );
+        fhr_widget.set_left_cr_frac(left_control_rod_insertion_frac);
+        fhr_widget.set_right_cr_frac(right_control_rod_insertion_frac);
+
+        ui.put(reactor_rectangle, fhr_widget);
+
+        let temp = right_downcomer_lower_temp;
+
+        let pipe_coordinate_chg = 
+            vec2(0.0, 100.0);
+
+        let pipe_11_start = 
+            vec2(
+                0.5 * reactor_rectangle.left() + 0.5 * reactor_rectangle.right(),
+                reactor_rectangle.bottom() - reactor_rectangle.height() * 0.28,
+            );
+
+        let pipe_2_start = 
+            vec2(
+                pipe_11_start.x + pipe_coordinate_chg.x,
+                pipe_11_start.y + pipe_coordinate_chg.y,
+            );
+
+
+        let pipe_11_rect = 
+            egui::Rect {
+                min: Pos2 { x: 0.0, y: 0.0 } + pipe_11_start,
+                max: Pos2 { x: 0.0, y: 0.0 } + pipe_2_start,
+            };
+
+
+
+        let pipe_11_widget = SinglePipe::new(
+            pipe_coordinate_chg, 
+            min_temp, 
+            max_temp, 
+            temp
+        );
+        ui.put(pipe_11_rect, pipe_11_widget);
+
+        ui.separator();
+
+    }
+
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
+
+
+
+pub mod local_widgets_and_buttons;
+
+pub mod side_panel;
+
+pub mod panel_enum;
+
+pub mod graph_data;
+
+pub mod graph_pages;
---
+++ examples/fhr_sim_v1/main.rs
@@ -0,0 +1,255 @@
+use std::{sync::{Arc, Mutex}, thread};
+
+use uom::si::{f64::*, power::kilowatt};
+
+use crate::app::{graph_data::PagePlotData, panel_enum::Panel};
+
+
+/// this represents the first iteration 
+/// of the fhr simulator
+///
+/// basically one can do a FHR loop 
+/// with a permenantly steady state steam cycle
+/// the latter uses the tampines-steam-tables
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+fn main(){
+
+    fhr_simulator_v1().unwrap();
+
+
+}
+pub fn fhr_simulator_v1() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 800.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "FHR Core / Primary Simulator V1 Powered by TUAS and teh-o-prke",
+        native_options,
+        Box::new(|cc| {
+            // image support,
+            // from 
+            // https://github.com/emilk/egui/tree/master/examples/images
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::new(FHRSimulatorApp::new(cc)))
+
+    }
+
+        ),
+    )
+}
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[derive(Clone, Debug)]
+pub struct FHRSimulatorApp {
+
+    pub fhr_state: Arc<Mutex<FHRState>>,
+
+    /// what panel is open
+    pub open_panel: Panel,
+
+    #[serde(skip)]
+    /// pointer for plotting 
+    pub fhr_simulator_ptr_for_plotting: Arc<Mutex<PagePlotData>>
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[derive(Clone,Copy, Debug)]
+pub struct FHRState {
+    /// left control rod insertion fraction
+    pub left_cr_insertion_frac: f32,
+    /// right control rod insertion fraction
+    pub right_cr_insertion_frac: f32,
+
+    // temperatures for both reactor feedback and display
+    pub pebble_core_temp_degc: f64,
+    pub pebble_bed_coolant_temp_degc: f64,
+    pub core_bottom_temp_degc: f64,
+    pub core_top_temp_degc: f64,
+    pub core_inlet_temp_degc: f64,
+    pub core_outlet_temp_degc: f64,
+    pub left_downcomer_upper_temp_degc: f64,
+    pub left_downcomer_mid_temp_degc: f64,
+    pub left_downcomer_lower_temp_degc: f64,
+    pub right_downcomer_upper_temp_degc: f64,
+    pub right_downcomer_mid_temp_degc: f64,
+    pub right_downcomer_lower_temp_degc: f64,
+
+    // for diagnostics
+    /// this displays reactor thermal power in megawatts,
+    /// including decay heat
+    pub reactor_power_megawatts: f64,
+    /// this is decay heat in megawatts 
+    pub reactor_decay_heat_megawatts: f64,
+    /// this displays reactor keff
+    pub keff: f64,
+    /// this displays reactivity in dollars 
+    pub reactivity_dollars: f64,
+    /// this displays xenon feedback in dollars 
+    pub xenon135_feedback_dollars: f64,
+
+    // this is important for coupling between prke loop and thermal 
+    // hydraulics loop
+    pub prke_loop_accumulated_timestep_seconds: f64,
+    pub prke_loop_accumulated_heat_removal_kilojoules: f64,
+
+    /// pump pressure settings 
+    pub fhr_pri_loop_pump_pressure_kilopascals: f64,
+    pub fhr_intermediate_loop_pump_pressure_kilopascals: f64,
+
+
+    // this is important for timestep monitoring 
+    // time diagnostics
+    pub prke_simulation_time_seconds: f64,
+    pub prke_elapsed_time_seconds: f64,
+    pub prke_calc_time_microseconds: f64,
+    pub prke_timestep_microseconds: f64,
+
+    pub thermal_hydraulics_simulation_time_seconds: f64,
+    pub thermal_hydraulics_calc_time_microseconds: f64,
+    pub thermal_hydraulics_timestep_microseconds: f64,
+
+
+    // diagnostics for thermal hydraulics loop 
+    pub reactor_branch_flowrate_kg_per_s: f64,
+    pub downcomer1_branch_flowrate_kg_per_s: f64,
+    pub downcomer2_branch_flowrate_kg_per_s: f64,
+    pub ihx_branch_flowrate_kg_per_s: f64,
+    pub intermediate_loop_clockwise_flow_kg_per_s: f64,
+}
+
+impl Default for FHRState {
+    fn default() -> Self {
+        let default_temperature_degc = 500.0;
+        FHRState { 
+            left_cr_insertion_frac: 0.40,
+            right_cr_insertion_frac: 0.40,
+            pebble_core_temp_degc: default_temperature_degc,
+            pebble_bed_coolant_temp_degc: default_temperature_degc,
+            core_bottom_temp_degc: default_temperature_degc,
+            core_top_temp_degc: default_temperature_degc,
+            core_inlet_temp_degc: default_temperature_degc,
+            core_outlet_temp_degc: default_temperature_degc,
+            left_downcomer_upper_temp_degc: default_temperature_degc,
+            left_downcomer_mid_temp_degc: default_temperature_degc,
+            left_downcomer_lower_temp_degc: default_temperature_degc,
+            right_downcomer_upper_temp_degc: default_temperature_degc,
+            right_downcomer_mid_temp_degc: default_temperature_degc,
+            right_downcomer_lower_temp_degc: default_temperature_degc,
+            prke_loop_accumulated_timestep_seconds: 0.0,
+            prke_loop_accumulated_heat_removal_kilojoules: 0.0,
+            reactor_power_megawatts: 0.0,
+            keff: 0.0,
+            reactivity_dollars: 0.0,
+            xenon135_feedback_dollars: 0.0,
+            prke_simulation_time_seconds: 0.0,
+            prke_elapsed_time_seconds: 0.0,
+            prke_calc_time_microseconds: 0.0,
+            prke_timestep_microseconds: 0.0,
+            reactor_decay_heat_megawatts: 0.0,
+            fhr_pri_loop_pump_pressure_kilopascals: 100.0,
+            fhr_intermediate_loop_pump_pressure_kilopascals: 100.0,
+            thermal_hydraulics_simulation_time_seconds: 0.0,
+            thermal_hydraulics_calc_time_microseconds: 0.0,
+            thermal_hydraulics_timestep_microseconds: 0.0,
+            reactor_branch_flowrate_kg_per_s: 0.0,
+            downcomer1_branch_flowrate_kg_per_s: 0.0,
+            downcomer2_branch_flowrate_kg_per_s: 0.0,
+            ihx_branch_flowrate_kg_per_s: 0.0,
+            intermediate_loop_clockwise_flow_kg_per_s: 0.0,
+        }
+    }
+}
+
+impl FHRState {
+
+    pub fn obtain_average_heat_removal_rate_from_pebble_bed_and_reset_counter(
+        &mut self) -> Power {
+        let heat_removal_rate_kilowatts = 
+            self.prke_loop_accumulated_heat_removal_kilojoules/
+            self.prke_loop_accumulated_timestep_seconds;
+
+        self.prke_loop_accumulated_timestep_seconds = 0.0;
+        self.prke_loop_accumulated_heat_removal_kilojoules = 0.0;
+        return Power::new::<kilowatt>(heat_removal_rate_kilowatts);
+    }
+}
+
+
+impl FHRSimulatorApp {
+    /// Called once before the first frame.
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        //// Load previous app state (if any).
+        //// Note that you must enable the `persistence` feature for this to work.
+        //if let Some(storage) = cc.storage {
+        //    return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        //}
+
+        let new_fhr_app: FHRSimulatorApp = Default::default();
+
+        let fhr_state_prke_ptr: Arc<Mutex<FHRState>> = 
+            new_fhr_app.fhr_state.clone();
+        let fhr_state_thermal_hydraulics_ptr: Arc<Mutex<FHRState>> = 
+            new_fhr_app.fhr_state.clone();
+        // these are pointers/references for plotting reactor power 
+        // both the instantaneous state 
+        // and page plotting
+        let fhr_state_plot_ptr: Arc<Mutex<FHRState>> = 
+            new_fhr_app.fhr_state.clone();
+        let fhr_page_plot_ptr: Arc<Mutex<PagePlotData>> = 
+            new_fhr_app.fhr_simulator_ptr_for_plotting.clone();
+
+        // now spawn a thread to do the kinetics
+        //
+        thread::spawn(move ||{
+            // now I also have a PRKE data which lives inside this loop
+            FHRSimulatorApp::calculate_prke_loop(fhr_state_prke_ptr);
+        });
+
+        // spawn a thread to do the thermal hydraulics
+        thread::spawn(move ||{
+            FHRSimulatorApp::calculate_thermal_hydraulics_loop(
+                fhr_state_thermal_hydraulics_ptr
+            );
+            
+        });
+        // spawn a thread to do the updating of graph plots
+        thread::spawn(move ||{
+            FHRSimulatorApp::update_plot_from_fhr_state(
+                fhr_state_plot_ptr,
+                fhr_page_plot_ptr
+            );
+            
+        });
+
+        new_fhr_app
+    }
+
+
+    
+}
+impl Default for FHRSimulatorApp {
+    fn default() -> Self {
+
+        let fhr_state = FHRState::default();
+        let fhr_state_ptr = Arc::new(Mutex::new(fhr_state));
+        let fhr_plot: PagePlotData = PagePlotData::default();
+        let fhr_plot_ptr = Arc::new(Mutex::new(fhr_plot));
+        let default_open_panel = Panel::MainPage;
+
+        Self {
+            fhr_state: fhr_state_ptr,
+            open_panel: default_open_panel,
+            fhr_simulator_ptr_for_plotting: fhr_plot_ptr,
+
+        }
+    }
+}
+
+pub mod app;
