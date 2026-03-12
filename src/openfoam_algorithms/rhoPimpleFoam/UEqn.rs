3c586afece2b2b29d8abe2e81bd2250e5b3e3ec2
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-03-12 12:57:42 +0800

    added a h times delta t into this

--- src/openfoam_algorithms/rhoPimpleFoam/UEqn.rs
+++ src/openfoam_algorithms/rhoPimpleFoam/UEqn.rs
@@ -24,7 +24,7 @@
 //    K = 0.5*magSqr(U);
 //}
 //
-use uom::si::f64::*;
+use uom::{ConstZero, si::{angle::radian, f64::*}};
 
 /// within UEqn, we work with mass flowrates
 /// note: this wasn't vibe coded
@@ -41,6 +41,8 @@ pub struct UEqn {
     cross_sectional_area_vec_last_iter: Vec<Area>,
     /// again, dz = sin theta dx
     /// so dz/dx = sin theta
+    ///
+    /// theta is the angle of incline
     theta_vec: Vec<Angle>,
     /// this is length of each component
     dx: Vec<Length>,
@@ -66,4 +68,51 @@ impl UEqn {
 
         todo!()
     }
+
+
+    /// this gives a vector of H
+    ///
+    /// H = mass_flowrate/delta_t 
+    /// - rho g A_{XS} dz/dx
+    ///
+    /// there is no common unit for mass flowrate/delta_t 
+    /// so i rather return 
+    ///
+    /// H * delta_t
+    pub fn H_times_delta_t(&self, include_transient_term: bool) -> Vec<MassRate> {
+
+        let vector_length: usize = self.mass_flowrate_vector_last_iter.len();
+        let mut h_times_delta_t_vec: Vec<MassRate> 
+            = vec![MassRate::ZERO; vector_length];
+        // obtain timestep
+        let delta_t = self.delta_t;
+        for (i, mass_rate_ptr) in self.mass_flowrate_vector_last_iter.iter().enumerate(){
+
+            let theta: Angle = self.theta_vec[i];
+
+            let rho: MassDensity = self.density_vector_last_iter[i];
+            let g: Acceleration = self.g;
+            let xs_area: Area = self.cross_sectional_area_vec_last_iter[i];
+
+            // dz/dx = sin theta, where theta is the angle of incline
+            let dz_dx: Ratio = theta.get::<radian>().sin().into();
+
+            let mut h = - rho * g * xs_area * dz_dx;
+
+            
+            if include_transient_term {
+
+                h += *mass_rate_ptr/delta_t;
+
+            }
+
+            h_times_delta_t_vec[i] = h * delta_t;
+
+
+
+        }
+        return h_times_delta_t_vec;
+    }
+
+    
 }
