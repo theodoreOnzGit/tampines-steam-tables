e2f805d3148809093cb8e26f81bc54c379a045c5
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2026-03-21 10:15:53 +0800

    added skeletal structure for fhr widget

--- examples/fhr_sim_v2/app/local_widgets_and_buttons/turbine_widget.rs
+++ examples/fhr_sim_v2/app/local_widgets_and_buttons/turbine_widget.rs
@@ -1,8 +1,13 @@
-use egui::Widget;
+use egui::{epaint::PathShape, vec2, Color32, Pos2, Sense, Stroke, Vec2, Widget};
+use egui::epaint::CubicBezierShape;
+use uom::si::{f64::*, thermodynamic_temperature::degree_celsius};
+
+use super::hot_to_cold_colour_mark_1;
 
 #[derive(Debug,Clone, PartialEq)]
 pub struct TurbineWidget {
 
+    size: Vec2,
 }
 
 // for the turbine widget, this is basically a collection of rectangles 
@@ -11,10 +16,24 @@ pub struct TurbineWidget {
 // These strokes will vary position with sine wave functions
 impl TurbineWidget {
 
+    /// gets the size of the widget 
+    pub fn size(&self) -> Vec2 {
+
+        self.size.clone()
+    }
 }
 
 impl Widget for TurbineWidget {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        todo!()
+        let size = self.size();
+        let (response, painter) = ui.allocate_painter(
+            size, Sense::hover()
+        );
+        // this will be the main bulk of code
+
+
+
+
+        return response;
     }
 }
