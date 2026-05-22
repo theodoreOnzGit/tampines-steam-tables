0f5de76bdf05f6e8c9759e2426125edc8954563b
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-05-22 13:56:04 +0800

    gotten the velocity scanning okay

--- src/steam_turbine_equations/converging_diverging_nozzles/joule_thomson.rs
+++ src/steam_turbine_equations/converging_diverging_nozzles/joule_thomson.rs
@@ -1,4 +1,4 @@
-use uom::si::f64::*;
+use uom::si::{f64::*, ratio::ratio};
 use crate::interfaces::object_oriented_programming::TampinesSteamTableCV;
 
 /// we want to obtain the outlet thermodynamic state of a flow 
@@ -33,27 +33,7 @@ pub fn get_outlet_state_joule_thomson(
     // stagnation enthalpy
     let h0: AvailableEnergy = h1 + 0.5 * v1 * v1;
 
-    // Helper: Calculate mass flowrate given outlet enthalpy (p,h) flash
-    fn calculate_mass_rate_at_outlet_ph(
-        h0: AvailableEnergy,
-        p2: Pressure,
-        h2: AvailableEnergy,
-        a2: Area,
-    ) -> MassRate {
-        // Energy equation: v₂ = √(2(h₀ - h₂))
-        let v2: Velocity = (2.0 * (h0 - h2)).sqrt();
-        
-        // Get density from (p,h) flash
-        let ref_vol = TampinesSteamTableCV::get_ref_vol();
-        let state_2 = TampinesSteamTableCV::new_from_ph(p2, h2, ref_vol);
-        let rho2 = state_2.get_rho();
-        
-        // Mass flux: G = ρv
-        // Mass rate: G*a2
-        let mass_rate = rho2 * v2 * a2;
-        
-        mass_rate
-    }
+
 
     // Helper: calculate mass flowrate using outlet enthalpy (p,h) flash 
     // using velocit as input
@@ -91,17 +71,62 @@ pub fn get_outlet_state_joule_thomson(
     // sign changes
 
     let mut test_rho = rho1;
+    let mut mass_rate_error_initial = 0.0;
+    let mut upper_bound_velocity: Velocity;
+    let mut lower_bound_velocity: Velocity;
 
     let max_iter = 50;
+    let debug = true;
 
     for i in 0..max_iter {
 
         // first let's test the mass flowrate and state 
         // by obtaining the velocity
 
+        let v2: Velocity = mass_flowrate/test_rho/a1;
+        let a2 = a1;
+
+        let (mass_rate_test, state_2_test) = 
+            calculate_mass_rate_and_state_at_outlet_ph_velocity(
+                h0, p2, v2, a2
+            );
+        
+        // check residual 
+        //
+        let mass_rate_error: f64 = 
+            ((mass_rate_test - mass_flowrate)/mass_flowrate)
+            .get::<ratio>();
+
+        // for ONLY the first iteration, we check the initial mass rate 
+        // error 
+
+        if i == 0 {
+            mass_rate_error_initial = mass_rate_error;
+        }
+
         
 
+        // mutate the test rho, to reduce it
+
+        if debug {
+            dbg!(&(test_rho,mass_rate_error,state_2_test));
+        }
+
+        // if there is sign change, break out 
+        // we capture the upper bound velocity
+
+        if mass_rate_error * mass_rate_error_initial <= 0.0 {
+            upper_bound_velocity = v2;
+            break;
+        }
+        // if there is no sign change, update the lower bound velocity 
+
+        lower_bound_velocity = v2;
+        test_rho *= 0.90;
+
     }
+    // now for sure, the velocity lies between the upper and lower bound
+    // we can use regula falsi to finish this job
 
     
 
