ab653aa9b61fb755bfd2bbf8aad8b090bc1bd9f9
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-05-25 09:22:42 +0800

    added references for the marikven tests

--- src/steam_turbine_equations/converging_diverging_nozzles/tests/marviken_tests.rs
+++ src/steam_turbine_equations/converging_diverging_nozzles/tests/marviken_tests.rs
@@ -16,17 +16,21 @@ use uom::si::velocity::meter_per_second;
 
 use crate::interfaces::object_oriented_programming::TampinesSteamTableCV;
 use crate::steam_turbine_equations::calculate_velocity_mass_flowrate_and_state_in_cd_nozzle;
-/// note: this is vibe coded using Gemini
-///
-/// probably start as a template 
-/// but i just want nozzle flow 
-///
+
 /// looks like fig 8:24 on page 113 seems to be the best 
 /// as most nozzles have pipe which may cause extra pressure loss and 
 /// reduce flowrate.
 ///
+/// These correspond to test 23 and 24. 
+///
 /// But Fig 8:24 has L/D at 0.3 
 /// which makes it as pure a nozzle as it gets
+///
+/// In the pdf, this is on page 100 (113 of the pdf)
+/// The conditions of test 23 and 24 are in table 4:2 on 
+/// page 21 (page 34 of the pdf)
+///
+/// Theri
 #[test]
 fn validate_against_marviken_test_24() {
     // --- Step 1: Define Initial Conditions from Table 4.1.1 ---
